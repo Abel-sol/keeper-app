169e432a3bc1c6a2dfbe7dff61de70e0bdd4e75b
Abel Solomon <abelsolab17@gmail.com>
2023-08-08 08:55:16 -0700

    added the neccesary code to map notes

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,10 +1,16 @@
 import React  from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Notes"
+import Note from "./Notes";
+import notes from "../notes";
 
 function App(){
-  return <div><Header /> <Note /> <Footer /></div>
+  return <div><Header /> {notes.map( note => (
+    <Note
+     title = {note.title} 
+     content = {note.content}
+     />)
+  )} <Footer /></div>
 }
 
 export default App;
--- src/component/Notes.jsx
+++ src/component/Notes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Note(prop){
-  return <div className="note"><h1>{prop.title}</h1><p>{prop.contnet}</p></div> ;
+  return <div className="note"><h1>{prop.title}</h1><p>{prop.content}</p></div> ;
 }
 
 export default Note;
