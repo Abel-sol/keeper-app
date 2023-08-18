8691519a0e1a9bd7b31f5b67b2d9ec61d3587a35
Abel Solomon <abelsolab17@gmail.com>
2023-08-18 08:08:05 -0700

    added CreateNote component to App

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -3,9 +3,13 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Notes";
 import notes from "../notes";
+import CreateNote from "../component/CreateNote";
 
 function App(){
-  return <div><Header /> {notes.map( note => (
+  return <div>
+    <Header /> 
+     <CreateNote/>
+    {notes.map( note => (
     <Note
      key = {note.key}
      title = {note.title} 
--- src/component/CreateNote.jsx
+++ src/component/CreateNote.jsx
@@ -10,3 +10,5 @@ function CreateNote(){
     </form>
   </div>)
 }
+
+export default CreateNote;
